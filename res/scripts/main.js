3169a052a44fd4ad442d45acaffefa9be2223d91
webdevavi96 <avinaschaurasiya902@gmail.com>
2025-03-04 23:49:08 +0530

    update

--- res/scripts/main.js
+++ res/scripts/main.js
@@ -14,3 +14,48 @@ window.onload = () => {
     }
   }, 1000); // 1 second delay before hiding the loader
 };
+
+
+
+const popUpButtons = document.querySelectorAll('.source_code');
+
+popUpButtons.forEach((button) => {
+  button.addEventListener('click', (event) => {
+    event.preventDefault();
+    
+    let existingCodeBlock = button.nextElementSibling;
+    if (existingCodeBlock && existingCodeBlock.classList.contains('popOverTarget')) {
+      existingCodeBlock.remove();
+      return;
+    }
+    
+    let div = document.createElement('div');
+    div.classList.add('popOverTarget');
+    
+    let codeBlock = document.createElement('code');
+    
+    let projectTitle = button.parentElement.querySelector('h2').innerText;
+    let sourceCodeMessage = "";
+    
+    switch (projectTitle) {
+      case "Travel Vista":
+        sourceCodeMessage = "Source code for Travel Vista: \nGitHub: git clone https://github.com/webdevavi96/myproject.git";
+        break;
+      case "Portfolio Website":
+        sourceCodeMessage = "Source code for Portfolio Website: \nGitHub: git clone https://github.com/webdevavi96/My-Portfolio.git";
+        break;
+      case "Netflix Clone":
+        sourceCodeMessage = "Source code for Netflix Clone is currently unavailable.";
+        break;
+      case "Alumni Meet Web App":
+        sourceCodeMessage = "Source code for Alumni Meet Web App: \nAvailable soon...";
+        break;
+      default:
+        sourceCodeMessage = "Source code information not available.";
+    }
+    
+    codeBlock.textContent = sourceCodeMessage;
+    div.appendChild(codeBlock);
+    button.after(div);
+  });
+});
